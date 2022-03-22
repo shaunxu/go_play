49c32b7f6e3fa361adc35e5b2265c0d7c6d786bb
Shaun Xu <jfarrio@gmail.com>
2022-03-22 16:37:39 +0800

    .

---
+++ go.mod
@@ -0,0 +1,5 @@
+module shuanxu.com/go_play/v2
+
+go 1.17
+
+require golang.org/x/tour v0.1.0 // indirect
---
+++ switch.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+// 返回一个“返回int的函数”
+func fibonacci() func() int {
+	
+}
+
+func main() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
